5c61298f7f38a887e790ac27092281bc28bfea6c
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2019-11-08 20:22:53 +0530

    initial commit

---
+++ dotsnboxes/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Dots from './Component/Dot/Dot';
+
+function App() {
+  return (
+    <div className="App">
+      <Dots/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ dotsnboxes/src/Component/Dot/Dot.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Dot.css';
+const Dot = (props) => {
+    const dots = Array(9).fill(null);
+
+    const displayDot = (index) => {
+        return (
+            <span className="dot"></span>
+
+        );
+    }
+
+
+    return (
+        <table >
+        <tr className="dot-row" border="1px">
+            {displayDot(0)}
+            {displayDot(1)}
+            {displayDot(2)}
+        </tr>
+
+        <tr>
+            {displayDot(3)}
+            {displayDot(4)}
+            {displayDot(5)}
+        </tr>
+
+        <tr>
+            {displayDot(6)}
+            {displayDot(7)}
+            {displayDot(8)}
+        </tr>
+     </table>
+    );
+
+
+}
+
+export default Dot;
