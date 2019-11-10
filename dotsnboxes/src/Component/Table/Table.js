33e510a7d4444fe6981c0adbe73db39c2dd1934a
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2019-11-10 17:04:32 +0530

    object of state not updating properly

--- dotsnboxes/src/Component/Table/Table.js
+++ dotsnboxes/src/Component/Table/Table.js
@@ -1,33 +1,63 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import './Table.css'
 import Td from '../TableData/Td';
 
 const Test=(props)=>  {
 
-   
+   const [playerTurn,setplayerTurn]=useState(true);
+   const [box,setBox]=useState({
+     index:null,
+     l:'',
+     r:'',
+     t:'',
+     b:''
+   })
+   const [checkPlayer,setCheckPlayer]=useState(true);
+   let player=playerTurn?'Player1':'player2';
 
    const clicked = (e,index) => {
+
+
+
       if (e.nativeEvent.offsetX < 2) {
        let x=document.getElementsByClassName("data");
        x[index].style.borderLeft = "thick solid #0000FF"
         console.log('left')
         console.log(e.nativeEvent.offsetX);
+        setBox({...box,l:'visited',index:index});
+        setplayerTurn(!playerTurn);
       }else if (e.nativeEvent.offsetY<2){
         console.log('top')
         let x=document.getElementsByClassName("data");
        x[index].style.borderTop = "thick solid #0000FF"
+       setBox({...box,t:'visited',index:index});
+       setplayerTurn(!playerTurn);
+
       }
       else if (e.nativeEvent.offsetX>50) {
         console.log('right')
         console.log(e.nativeEvent.offsetX)
         let x=document.getElementsByClassName("data");
        x[index].style.borderRight = "thick solid #0000FF"
+       setBox({...box,r:'visited',index:index});
+       setplayerTurn(!playerTurn);
+
       } else if (e.nativeEvent.offsetY>50) {
         console.log('bottom')
         console.log(e.nativeEvent.offsetY)
         let x=document.getElementsByClassName("data");
        x[index].style.borderBottom = "thick solid #0000FF"
+       setBox({...box,b:'visited',index:index});
+       setplayerTurn(!playerTurn);
+
       }
+      
+      
+      // if(checkPlayer===playerTurn){
+      // setplayerTurn(!playerTurn);
+      
+      // }
+      console.log("boxxxxxx",box);
     }
 
 const displayTd=(index)=>{
@@ -39,6 +69,8 @@ const displayTd=(index)=>{
     
       
       return (
+        <>
+      Its time of {player}
         <table className="table">
             <tbody>
        
@@ -63,6 +95,7 @@ const displayTd=(index)=>{
         
         </tbody>
               </table>
+              </>
       );
     }
     
