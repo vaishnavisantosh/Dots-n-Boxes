dfe182cc815d6687c9c461553c645d2c12e4010f
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2019-11-11 11:03:32 +0530

    values get updated as per border click

--- dotsnboxes/src/Component/Table/Table.js
+++ dotsnboxes/src/Component/Table/Table.js
@@ -1,137 +1,231 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Table.css'
 import Td from '../TableData/Td';
 
-const Test=(props)=>  {
+const Test = (props) => {
 
-   const [playerTurn,setplayerTurn]=useState(true);
-   const [boxcreated,setBoxCreated]=useState(false);
-   const [box,setBox]=useState({
-     index:null,
-     l:'',
-     r:'',
-     t:'',
-     b:''
-   })
-   const [boxfill,setBoxFill]=useState(true);
-   let player=playerTurn?'Player1':'player2';
+  const [playerTurn, setplayerTurn] = useState(true);
+ // const [boxcreated, setBoxCreated] = useState(false);
+  const [box, setBox] = useState([
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    },
+    {
+      index: null,
+      l: '',
+      r: '',
+      t: '',
+      b: ''
+    }]
 
-   const  checkBoxCreated=(resultData)=>{
-     if (resultData.l==="visited" && resultData.r==="visited"&& resultData.t==="visited"&& resultData.b==="visited"){
-     // setBoxCreated(true);
+  )
+ // const [boxfill, setBoxFill] = useState(true);
+  let player = playerTurn ? 'Player1' : 'player2';
+
+  const checkBoxCreated = (resultData) => {
+    if (resultData.l === "visited" && resultData.r === "visited" && resultData.t === "visited" && resultData.b === "visited") {
+      // setBoxCreated(true);
       //setBoxFill(false);
       console.log("created")
       return true;
-     }
-     return  false;
-   }
+    }
+    return false;
+  }
 
 
-   const  handleClick=(e,index)=>{
+  const handleClick = (e, index) => {
     let resultData;
     let boxcrated;
-    resultData= clicked(e,index);
-    boxcrated= checkBoxCreated(resultData);
-    if(boxcrated){
+    resultData = clicked(e, index);
+    boxcrated = checkBoxCreated(resultData);
+    if (boxcrated) {
       setplayerTurn(playerTurn);
     }
-    
-   }
 
-   const clicked = (e,index) => {
-    
-    let result={...box};
-  
+  }
 
-      if (e.nativeEvent.offsetX < 2) {
-       let x=document.getElementsByClassName("data");
-       x[index].style.borderLeft = "thick solid #0000FF"
-        console.log('left')
-        console.log(e.nativeEvent.offsetX);
-        setBox({...box,l:'visited',index:index});
+  const clicked = (e, index) => {
+
+    let result = { ...box };
+
+
+    if (e.nativeEvent.offsetX < 2) {
+      let x = document.getElementsByClassName("data");
+      x[index].style.borderLeft = "thick solid #0000FF"
+      console.log('left')
+      console.log(e.nativeEvent.offsetX);
+      if (box[index].l != "visited") {
         setplayerTurn(!playerTurn);
+        result[index].l="visited";
+        result[index].index=index;
+        setBox(result);
 
-      result={...box,l:'visited',index:index}
-      }else if (e.nativeEvent.offsetY<2){
-        console.log('top')
-        let x=document.getElementsByClassName("data");
-       x[index].style.borderTop = "thick solid #0000FF"
-       setBox({...box,t:'visited',index:index});
-       setplayerTurn(!playerTurn);
-       result={...box,t:'visited',index:index}
-
+        result = { ...box}
       }
-      else if (e.nativeEvent.offsetX>50) {
-        console.log('right')
-        console.log(e.nativeEvent.offsetX)
-        let x=document.getElementsByClassName("data");
-       x[index].style.borderRight = "thick solid #0000FF"
-       setBox({...box,r:'visited',index:index});
-       setplayerTurn(!playerTurn);
-       result={...box,r:'visited',index:index}
-
-      } else if (e.nativeEvent.offsetY>50) {
-        console.log('bottom')
-        console.log(e.nativeEvent.offsetY)
-        let x=document.getElementsByClassName("data");
-       x[index].style.borderBottom = "thick solid #0000FF"
-       setBox({...box,b:'visited',index:index});
-       setplayerTurn(!playerTurn);
-       result={...box,b:'visited',index:index}
-
+      else {
+        alert("already visited please choose another line")
+        setplayerTurn(playerTurn);
       }
-      
-      return(result)
-    
-      
+
+    } else if (e.nativeEvent.offsetY < 2) {
+      console.log('top')
+      let x = document.getElementsByClassName("data");
+      x[index].style.borderTop = "thick solid #0000FF"
+      if (box[index].t != "visited") {
+        result[index].t="visited";
+        result[index].index=index;
+        setBox(result);
+
+       // setBox({ ...box, t: 'visited', index: index });
+        setplayerTurn(!playerTurn);
+        result = { ...box}
+      }
+      else {
+        alert("already visited please choose another line")
+        setplayerTurn(playerTurn);
+      }
     }
-    
+    else if (e.nativeEvent.offsetX > 50) {
+      console.log('right')
+      console.log(e.nativeEvent.offsetX)
+      let x = document.getElementsByClassName("data");
+      x[index].style.borderRight = "thick solid #0000FF"
+      if (box.r != "visited") {
+        result[index].r="visited";
+        result[index].index=index;
+        setBox(result);
+      //  setBox({ ...box, r: 'visited', index: index });
+        setplayerTurn(!playerTurn);
+        result = { ...box}
+      }
+      else {
+        alert("already visited please choose another line")
+        setplayerTurn(playerTurn);
+      }
 
-   
-   // console.log("boxxxxxx",box);
+    } else if (e.nativeEvent.offsetY > 50) {
+      console.log('bottom')
+      console.log(e.nativeEvent.offsetY)
+      let x = document.getElementsByClassName("data");
+      x[index].style.borderBottom = "thick solid #0000FF"
+      if (box.b != "visited") {
+        result[index].b="visited";
+        result[index].index=index;
+        setBox(result);
+        //setBox({ ...box, b: 'visited', index: index });
+        setplayerTurn(!playerTurn);
+        result = { ...box}
+      }
+      else {
+        alert("already visited please choose another line")
+        setplayerTurn(playerTurn);
+      }
+    }
 
-const displayTd=(index)=>{
+    return (result)
+
+
+  }
+
+
+
+  // console.log("boxxxxxx",box);
+
+  const displayTd = (index) => {
+    return (
+      <Td value={player} onClick={(e) => handleClick(e, index)} />
+    );
+  }
+
+
+
   return (
-    <Td  onClick={(e) => handleClick(e,index)} />
-);
-}
-
-    
-      
-      return (
-        <>
+    <>
 
       Its time of {player}
-     
-        <table className="table">
-            <tbody>
-       
-         <tr>
-           {displayTd(0)}
-           {displayTd(1)}
-           {displayTd(2)}
-           
-         </tr>
-         <tr>
-           {displayTd(3)}
-           {displayTd(4)}
-           {displayTd(5)}
-          
-         </tr>
-         <tr>
-           {displayTd(6)}
-           {displayTd(7)}
-           {displayTd(8)}
-          
-         </tr>
-        
-        </tbody>
-              </table>
-              </>
-      );
-    }
-    
 
-  
-  export default Test;
+      <table className="table">
+        <tbody>
+
+          <tr>
+            {displayTd(0)}
+            {displayTd(1)}
+            {displayTd(2)}
+
+          </tr>
+          <tr>
+            {displayTd(3)}
+            {displayTd(4)}
+            {displayTd(5)}
+
+          </tr>
+          <tr>
+            {displayTd(6)}
+            {displayTd(7)}
+            {displayTd(8)}
+
+          </tr>
+
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+
+
+export default Test;
  // React.render(<App />, document.getElementById('app'));
